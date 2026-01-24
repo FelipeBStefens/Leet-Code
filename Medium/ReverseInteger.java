d8c783dd8e68310e7beb3dc139099fbefcdb1571
Felipe Bianchi Stefens <lipebianchistefens@gmail.com>
2026-01-24 15:19:49 -0300

    Reverse Integer

---
+++ Medium/ReverseInteger.java
@@ -0,0 +1,20 @@
+package Medium;
+
+public class ReverseInteger {
+    
+    public int reverse(int x) {
+
+        long reverseNumber = 0;
+
+        while(x != 0) {
+
+            reverseNumber = (reverseNumber * 10) + (x % 10);
+            x /= 10; 
+        }
+
+        if (reverseNumber > Integer.MAX_VALUE || reverseNumber < Integer.MIN_VALUE) {
+            return 0;
+        }
+        return (int) reverseNumber;
+    }
+}
