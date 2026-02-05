9c346f50d006b686a420b1b9013099038c952bf9
Felipe Bianchi Stefens <lipebianchistefens@gmail.com>
2026-02-05 16:47:21 -0300

    Transformed Array Exercise

---
+++ Easy/TransformedArray.java
@@ -0,0 +1,16 @@
+package Easy;
+
+public class TransformedArray {
+    
+    public int[] constructTransformedArray(int[] nums) {
+        
+        int[] result = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+
+            result[i] = nums[((i + nums[i]) % nums.length + nums.length) % nums.length];
+        }
+
+        return result;
+    }
+}
